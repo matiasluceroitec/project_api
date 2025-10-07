48e04626e9d74d78ee2caa2d035246457d4952b3
matias <matiasjavierlucero@gmail.com>
2025-10-07 20:08:18 -0300

    login schema

--- schemas.py
+++ schemas.py
@@ -24,3 +24,14 @@ class UserSchema(Schema):
         ),
         dump_only=True
     )
+
+class RegisterSchema(Schema):
+    name = fields.Str(required=True)
+    email = fields.Email(required=True)
+    password = fields.Str(required=True, load_only=True)
+    role = fields.Str(load_only=True)
+
+class LoginSchema(Schema):
+    email = fields.Email(required=True)
+    password = fields.Str(required=True, load_only=True)
+    
